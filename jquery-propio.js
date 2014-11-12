0d32aaf1437d7d090b549cc0337e6b743022b478
sebastian <sebas03700@hotmail.com>
2014-11-12 13:16:59 -0500

    - Slider
    - Se agrego un slider
    - Hay un problema con etiquetas

--- jquery-propio.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $("a").mouseover(function(event){
-      $("#capa").addClass("active");
-   });
-   $("a").mouseout(function(event){
-      $("#capa").removeClass("active");
-   });
-});
