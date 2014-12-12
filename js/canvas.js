856fca827837b917fd0ce9be9b014f747c8e49ee
admont28 <admont28@gmail.com>
2014-12-11 22:30:43 -0500

    - Avance Home (Sin terminar)
    - Se realiza un avance en el Home, falta organizar el diseño del Footer.

--- js/canvas.js
+++ js/canvas.js
@@ -1,22 +1,21 @@
 function cargador(){
-	var lienzo = document.getElementById('lienzo');
-	var lienzo1 = lienzo.getContext('2d');
-		// gradiente lineal horizontal
-	var gradienteLineal = lienzo1.createLinearGradient(0,0,220,0); 
-	gradienteLineal.addColorStop(0, '#8DB342');
-    gradienteLineal.addColorStop(1, '#229E4D');
-    lienzo1.fillStyle = gradienteLineal;
-
-  	lienzo1.beginPath();
-	lienzo1.moveTo(0,0);
-	lienzo1.lineTo(300,0);
-	lienzo1.lineTo(300,110);
-	lienzo1.lineTo(285,110);
-	lienzo1.lineTo(280,90);
-	lienzo1.lineTo(20,90);
-	lienzo1.lineTo(15,110);
-	lienzo1.lineTo(0,110);	
-	lienzo1.lineTo(0,0);
-	lienzo1.closePath();
-	lienzo1.fill();
+		
+		var canvas = document.getElementById('lienzo');
+        var lienzo1 = canvas.getContext('2d');
+          // a. Forma abierta
+        var gradienteLineal = lienzo1.createLinearGradient(0,0,1200,0); 
+		gradienteLineal.addColorStop(0, '#8DB342');
+	    gradienteLineal.addColorStop(1, '#009B62');
+	    lienzo1.fillStyle = gradienteLineal;
+		lienzo1.beginPath();
+		lienzo1.moveTo(0,0);
+		lienzo1.lineTo(1300,0);
+		lienzo1.lineTo(1300,600);
+		lienzo1.lineTo(1270,600);
+		lienzo1.lineTo(1230,530);
+		lienzo1.lineTo(70,530);
+		lienzo1.lineTo(30,600);
+		lienzo1.lineTo(0,600);	
+		lienzo1.closePath();
+		lienzo1.fill();
 }
