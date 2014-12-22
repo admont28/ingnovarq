a2ab6e8c40e4a49c7db3b1e2798fc68e58c1ad96
Andrés David Montoya Aguirre <admont28@gmail.com>
2014-12-22 18:30:34 -0500

    - Menú, Slider, Contenido, SocialMedia, Responsive
    - Se mejora el menú responsive, aún no se ha terminado.
    
    - Se cambia de slider con mejores efectos.
    
    - Se realiza responsive para el home.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,64 @@
- $(document).ready(function(){
- 	
-		        $('.myCarousel').carousel({
-		        	interval: 1000
-		        });	
-
-        $('.navbar-default').scrollToFixed();
-
-});
-
+/* Script para twitter */
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+/* Script para slider */
+$(function() {
+				
+				var Page = (function() {
+
+					var $navArrows = $( '#nav-arrows' ).hide(),
+						$navOptions = $( '#nav-options' ).hide(),
+						$shadow = $( '#shadow' ).hide(),
+						slicebox = $( '#sb-slider' ).slicebox( {
+							onReady : function() {
+
+								$navArrows.show();
+								$navOptions.show();
+								$shadow.show();
+
+							},
+						} ),
+						
+						init = function() {
+							initEvents();
+						},
+						initEvents = function() {
+
+							// add navigation events
+							$navArrows.children( ':first' ).on( 'click', function() {
+
+								slicebox.next();
+								return false;
+
+							} );
+
+							$navArrows.children( ':last' ).on( 'click', function() {
+								
+								slicebox.previous();
+								return false;
+
+							} );
+
+							$( '#navPlay' ).on( 'click', function() {
+								
+								slicebox.play();
+								return false;
+
+							} );
+
+							$( '#navPause' ).on( 'click', function() {
+								
+								slicebox.pause();
+								return false;
+
+							} );
+
+						};
+
+						return { init : init };
+
+				})();
+
+				Page.init();
+
+			});
