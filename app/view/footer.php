3943dd9fff9214d2b92b00e44ac67164359d7e5f
sebastian <sebas03700@hotmail.com>
2014-12-23 14:39:23 -0500

    - footer
    - footer: se paso el footer a un archivo php aparte

--- app/view/footer.php
+++ app/view/footer.php
@@ -1,3 +1,42 @@
 <?php 
+	function getFooter(){
+		?>
 
+		<footer>
+			<div class="col-xs-12 col-sm-12 col-md-12 footer">
+				<div  class=" col-xs-6 col-sm-6 col-md-6">
+					<div class="panel-heading">SERVICIOS</div>
+					 <div class="panel-body">
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN - 
+					  </div>
+				</div>
+				<div  class=" col-xs-6 col-sm-6 col-md-6">
+					<div class="panel-heading">CONTACTO</div>
+					 <div class="panel-body">
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						SITIO WEB EN CONSTRUCCIÓN -
+						
+					  </div>
+				</div>
+			</div>
+		</footer>
+
+		<?php
+	}
 ?>
--- app/view/inicio.php
+++ app/view/inicio.php
@@ -100,43 +100,8 @@
 				</div>
 			</div>
 		</div>
-	    	
-		<footer>
-			<div class="col-xs-12 col-sm-12 col-md-12 footer">
-				<div  class=" col-xs-6 col-sm-6 col-md-6">
-					<div class="panel-heading">SERVICIOS</div>
-					 <div class="panel-body">
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN - 
-					  </div>
-				</div>
-				<div  class=" col-xs-6 col-sm-6 col-md-6">
-					<div class="panel-heading">CONTACTO</div>
-					 <div class="panel-body">
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						
-					  </div>
-				</div>
-			</div>
-		</footer>
-	
-		<?php 
+		<?php
+			getFooter();
 			getImportsDown();
 		?>
 	</body>
