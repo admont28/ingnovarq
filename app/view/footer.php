b54c759a0105b8835425a00734997d6ea487f08a
sebastian <sebas03700@hotmail.com>
2014-12-23 17:15:24 -0500

    - footer
    - footer: se tiene un footer previo

--- app/view/footer.php
+++ app/view/footer.php
@@ -3,36 +3,53 @@
 		?>
 
 		<footer>
-			<div class="col-xs-12 col-sm-12 col-md-12 footer">
-				<div  class=" col-xs-6 col-sm-6 col-md-6">
-					<div class="panel-heading">SERVICIOS</div>
-					 <div class="panel-body">
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN - 
-					  </div>
+			
+			<div class="row footer-contenedor fondo-cont-footer color-a">
+				<div class="col-md-6 col-xs-12 separador">
+					<h2 class="title-footer color-a">Servicios</h2>
+					<div class="footer-container">
+						<ul>
+							<li>Gerencia de Proyectos</li>
+							<li>Diseño Arquitectónico</li>
+							<li>Construcción</li>
+							<li>Ingeniería Civil</li>
+							<li>Carreteras</li>
+							<li>Ingeniería Hidráulica</li>
+							<li>Suministro de Agua</li>
+							<li>Planeamiento de Transporte</li>
+							<li>Tuberías y Tanques</li>
+							<li>Edificios</li>
+							<li>Mantenimiento</li>
+						</ul>
+					</div>
+				</div>				
+				<div class="col-md-6 col-xs-12">
+					<h2 class="title-footer color-a">Contáctenos</h2>
+					<div class="footer-container">
+						<ul>
+							<li>
+								<a class="color-a" href="http://www.ingnovarq.com.co/" target="_blank">
+									http://www.ingnovarq.com.co/
+								</a>
+							</li>
+							<li>Cra. 13 No 21 N-52 Oficina 202 Edificio Torreyana B/ Alameda Armenia-Quindío</li>
+							<li><a class="color-a" href="mailto:ventas@ingnovarq.com.co">ventas@ingnovarq.com.co</a></li>
+							<li>320 676 7399</li>	
+							<li>(6) 7497519</li>			
+						</ul>
+					</div>
+
 				</div>
-				<div  class=" col-xs-6 col-sm-6 col-md-6">
-					<div class="panel-heading">CONTACTO</div>
-					 <div class="panel-body">
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						SITIO WEB EN CONSTRUCCIÓN -
-						
-					  </div>
+				
+			</div>	
+			<div class="row footer-contenedor fondo-cont-footer color-a">
+				<div class="col-md-9 col-xs-12">
+					© 2014 Ingnovarq S.A.S. Todos los derechos reservados.
+				</div>
+				<div class="col-md-3 col-xs-12">
+					<div class="logo-footer">
+						<img src="../../images/logo.jpg" class="img-responsive">
+					</div>	
 				</div>
 			</div>
 		</footer>
