2d2c2ddb1651bc41f2668371f591abb01f6a5094
sebastian <sebas03700@hotmail.com>
2014-12-30 18:17:22 -0500

    -portafolio de servicios
    - se esta agregando un efecto para el portafolio de servicios

--- app/view/servicios.php
+++ app/view/servicios.php
@@ -17,6 +17,40 @@
 			</div>
 
 			<div class="contenido">
+				<div class="">
+            
+		            <div id="ib-top" class="ib-top">
+		                <h1>Nuestros Servicios <span>Ingnovarq S.A.S</span></h1>
+		            </div>
+		            <div id="ib-main-wrapper" class="ib-main-wrapper">
+		                <div class="ib-main">
+		                    
+							<a href="#" class="ib-content">
+		                        <div class="ib-teaser">
+		                            <h2>Welcome <span>Howdy, Stranger</span></h2>
+		                        </div>
+		                        <div class="ib-content-full">
+		                            <p class="color-p">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+		                            <p class="color-p">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+		                            <p class="color-p">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+		                        </div>
+		                    </a>
+							
+							<a href="#" class="ib-content">
+		                        <div class="ib-teaser">
+		                            <h2>Welcome <span>Howdy, Stranger</span></h2>
+		                        </div>
+		                        <div class="ib-content-full">
+		                            <p class="color-p">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+		                            <p class="color-p">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+		                            <p class="color-p">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+		                        </div>
+		                    </a>
+							
+							<div class="clr"></div>
+						</div><!-- ib-main -->
+		            </div><!-- ib-main-wrapper -->
+        		</div>
 				<div class="row">
 					<div class="col-xs-12 col-sm-8 col-md-8">
 						<div class="panel panel-default empresa">
